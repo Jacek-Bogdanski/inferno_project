09d23041bd9cfc558f62f2e0163b11b1e9ac26f9
Jacek-Bogdanski <stuff.jackozbogdanca@gmail.com>
2022-05-13 19:50:51 +0200

    add comments, move code to separate method

--- inferno/src/main/java/com/project/Simulation/Tank.java
+++ inferno/src/main/java/com/project/Simulation/Tank.java
@@ -1,14 +1,27 @@
 package com.project.Simulation;
 
-public class Tank extends MillitaryUnit{
-    int fuel;
-    public Tank(char team,int x,int y){
-        super(team,x,y);
-        fuel=50;
+public class Tank extends MilitaryUnit{
+    private Integer fuel;
+
+    /**
+     * Konstruktor
+     * @param team nazwa drużyny [char]
+     * @param position pozycja początkowa
+     */
+    public Tank(char team,Position position){
+        super(team,position);
+        this.setParams();
+    }
+
+    /**
+     * Metoda ustawiająca parametry początkowe obiektu
+     */
+    private void setParams(){
+        this.fuel=50;
         this.ammo=10;
         this.attackRange = 7;
         this.dmg=50;        //maksymalny dmg, pozniej podczas ataku bedzie losowa wartosc w takim zakresie
         this.hp=100;
-        this.velocity=3;
+        this.speed=3;
     }
 }
