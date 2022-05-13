657f6b7c4052b8e252dbea9616bb29cb3c7bf3ed
Jacek-Bogdanski <stuff.jackozbogdanca@gmail.com>
2022-05-13 19:48:28 +0200

    Add comments, isCollected

--- inferno/src/main/java/com/project/Simulation/Item.java
+++ inferno/src/main/java/com/project/Simulation/Item.java
@@ -1,6 +1,10 @@
 package com.project.Simulation;
 
+/**
+ * Klasa obiektu zbieranego z mapy przez postacie
+ */
 public class Item {
-    String type;        //ammo,food,fuel
-    int value;          //0-100
+    String type="";        //ammo,food,fuel
+    int value=0;          //0-100
+    boolean isCollected=false;
 }
