bcc67d9025bcad0550ab9cfef988ad5a43d9d01d
mKazmierczak <mkazmierczak024@gmail.com>
2022-05-19 23:26:26 +0200

    empty move method for tanks

--- inferno/src/main/java/com/project/Simulation/Tank.java
+++ inferno/src/main/java/com/project/Simulation/Tank.java
@@ -1,8 +1,10 @@
 package com.project.Simulation;
 
+import java.util.Random;
+
 public class Tank extends MilitaryUnit{
     private Integer fuel;
-
+    private final Random rand = new Random();
     /**
      * Konstruktor
      * @param team nazwa drużyny [char]
@@ -26,7 +28,23 @@ public class Tank extends MilitaryUnit{
     }
 
     @Override
-    public void move(){
+    public void move(Field[][] map,Position pos, int id,int mapSize){
+        int direction = rand.nextInt(3); // 0-gora 1-prawo 2-dol 3-prawo
+        int moveSpeed = rand.nextInt(2)+1; // losowanie ilosci kratek do przebycia dla testow niech bedzie 1-3 kratki
+        switch (direction){
+            case 0:{
+
+            }
+            case 1:{
+
+            }
+            case 2:{
+
+            }
+            case 3:{
+
+            }
+        }
 
     }
 
