1ebf500a5cfd84902e81e60b72c90ef4c9f1b651
Jacek-Bogdanski <stuff.jackozbogdanca@gmail.com>
2022-05-27 18:46:21 +0200

    change paramteres

--- inferno/src/main/java/com/project/Parameters.java
+++ inferno/src/main/java/com/project/Parameters.java
@@ -15,28 +15,28 @@ public class Parameters {
     public static Double SOLDIER_FOOD_USAGE_PROBABILITY = 0.1;
     public static Integer SOLDIER_SPEED = 3;
     public static Integer SOLDIER_ATTACK_RANGE = 3;
-    public static Integer SOLDIER_ATTACK_DAMAGE = 20;
-    public static Integer SOLDIER_HP = 50;
+    public static Integer SOLDIER_ATTACK_DAMAGE = 40;
+    public static Integer SOLDIER_HP = 70;
     public static Integer SOLDIER_AMMUNITION = 50;
-    public static Integer SOLDIER_FOOD = 50;
+    public static Integer SOLDIER_FOOD = 200;
 
     /*
      * Parametry artylerzysty
      */
     public static Integer GUNNER_SPEED = 0;
     public static Integer GUNNER_ATTACK_RANGE = 30;
-    public static Integer GUNNER_ATTACK_DAMAGE = 200;
-    public static Integer GUNNER_HP = 10;
+    public static Integer GUNNER_ATTACK_DAMAGE = 600;
+    public static Integer GUNNER_HP = 50;
     public static Integer GUNNER_AMMUNITION = 2000;
 
     /*
      * Parametry czołgu
      */
     public static Double TANK_FUEL_USAGE_PROBABILITY = 0.3;
-    public static Integer TANK_FUEL = 50;
+    public static Integer TANK_FUEL = 100;
     public static Integer TANK_SPEED = 1;
     public static Integer TANK_ATTACK_RANGE = 6;
-    public static Integer TANK_ATTACK_DAMAGE = 160;
+    public static Integer TANK_ATTACK_DAMAGE = 200;
     public static Integer TANK_HP = 500;
     public static Integer TANK_AMMUNITION = 200;
 }
